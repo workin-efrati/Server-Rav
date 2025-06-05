0eba1375a9e25ba911e67fe7df05e06ea4d476ae
AviadDerli <aviad@workin.co.il>
2025-06-05 12:26:41 +0300

    model : shut

--- shut/shut.model.js
+++ shut/shut.model.js
@@ -6,11 +6,11 @@ const shutSchema = new mongoose.Schema({
     title: { type: String },
     tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "tag" }],
     date: { type: Date, required: true, default: Date.now },
-    messagesId: [{ type: mongoose.Schema.Types.ObjectId, ref: "whatsappMessage" }],
+    messagesId: [{ type: mongoose.Schema.Types.ObjectId, ref: "whatsappMessage_e" }],
     fuqs: [{
         question: { type: String, required: true },
         answer: { type: String, required: true },
     }]
 }, { timestamps: true });
 
-export const shutModel = mongoose.model('shut', shutSchema)
+export const shutModel = mongoose.model('shut_e', shutSchema)
